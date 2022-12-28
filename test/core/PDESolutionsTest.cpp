25e0b7ddaa3a45aed3a3e70e57fcc811e6e5c435
AlePalu <alessandropalummo@gmail.com>
2022-12-28 22:44:02 +0100

    add advection-diffusion PDE tests for linear and quadratic finite elements

--- test/core/PDESolutionsTest.cpp
+++ test/core/PDESolutionsTest.cpp
@@ -153,7 +153,6 @@ TEST(PDESolutionsTest, LaplacianIsotropicOrder2) {
 }
 
 // check error of approximated solution by an order 2 FEM within theoretical expectations
-// uses a more usable callable forcing
 TEST(PDESolutionsTest, LaplacianIsotropicOrder2_CallableForce) {
   // exact solution    
   auto solutionExpr = [](SVector<2> x) -> double { 
@@ -194,4 +193,127 @@ TEST(PDESolutionsTest, LaplacianIsotropicOrder2_CallableForce) {
   DMatrix<double> error_ = solution_ex - pde_.solution();
   double error_L2 = (pde_.R0() * error_.cwiseProduct(error_)).sum();
   EXPECT_TRUE(error_L2 < DOUBLE_TOLERANCE);
+}
+
+// check error of approximated solution by an order 1 FEM for the advection-diffusion problem
+//    -\Laplacian(u) - \alpha*(du/dx) = \gamma sin(pi*y)   on \Omega
+//    u = 0   on boundary
+// where \Omega is the [0,1] x [0,1] 2D unit square
+TEST(PDESolutionsTest, AdvectionDiffusionIsotropicOrder1) {
+  // setup problem parameter
+  constexpr double pi = 3.14159265358979323846;
+  double alpha_ = 1.0;
+  double gamma_ = pi;   
+
+  // define exact solution
+  // - \gamma/(\pi^2)*(p*\exp{\lambda_1*x} + (1-p)*\exp{\lambda_2*x} - 1)*sin(\pi*y)
+  double lambda1 = -alpha_/2 - std::sqrt((alpha_/2)*(alpha_/2) + pi*pi);
+  double lambda2 = -alpha_/2 + std::sqrt((alpha_/2)*(alpha_/2) + pi*pi);
+
+  double p_ = (1-std::exp(lambda2))/(std::exp(lambda1)-std::exp(lambda2));
+  
+  auto solutionExpr = [&gamma_, &lambda1, &lambda2, &p_](SVector<2> x) -> double { 
+    return -gamma_/(pi*pi) * ( p_ * std::exp(lambda1 * x[0]) + (1 - p_) * std::exp(lambda2 * x[0]) - 1. ) * std::sin(pi * x[1]);
+  };
+  
+  // non-zero forcing term
+  auto forcingExpr = [&gamma_](SVector<2> x) -> double { 
+    return gamma_ * std::sin(pi * x[1]);
+  };
+  
+  // differential operator
+  SVector<2> beta_;
+  beta_ << -alpha_, 0.;
+  auto bilinearForm = 1.0*Laplacian() + Gradient(beta_);
+  // load sample mesh for order 1 basis
+  MeshLoader<Mesh2D<>> unit_square("unit_square");
+  PDE<2,2,1, decltype(bilinearForm), DMatrix<double>> 
+  pde_(unit_square.mesh);
+  pde_.setBilinearForm(bilinearForm);
+
+  // compute boundary condition and exact solution
+  DMatrix<double> nodes_ = unit_square.mesh.dofCoords();
+  DMatrix<double> solution_ex(nodes_.rows(), 1);
+
+  for(int i=0; i<nodes_.rows(); ++i){
+    solution_ex(i) = solutionExpr(nodes_.row(i));
+  }
+  // set dirichlet conditions        
+  DMatrix<double> dirichletBC = DMatrix<double>::Zero(nodes_.rows(), 1);
+  pde_.setDirichletBC(dirichletBC);
+  
+  // request quadrature nodes and evaluate forcing on them
+  DMatrix<double> quadrature_nodes = pde_.integrator().quadratureNodes(unit_square.mesh);
+  DMatrix<double> f = DMatrix<double>::Zero(quadrature_nodes.rows(), 1);
+  
+  for(int i=0; i<quadrature_nodes.rows(); ++i){
+    f(i) = forcingExpr(quadrature_nodes.row(i));
+  }
+  pde_.setForcing(f);
+  
+  // init solver and solve differential problem
+  pde_.init();
+  pde_.solve();
+  
+  // check computed error
+  DMatrix<double> error_ = solution_ex - pde_.solution();
+  double error_L2 = (pde_.R0() * error_.cwiseProduct(error_)).sum();
+  EXPECT_TRUE(error_L2 < 1e-5);
+}
+
+// check error of approximated solution by an order 2 FEM for the advection-diffusion problem
+//    -\Laplacian(u) - \alpha*(du/dx) = \gamma sin(pi*y)   on \Omega
+//    u = 0   on boundary
+// where \Omega is the [0,1] x [0,1] 2D unit square
+TEST(PDESolutionsTest, AdvectionDiffusionIsotropicOrder2) {
+  // define problem parameters
+  constexpr double pi = 3.14159265358979323846;
+  double alpha_ = 1.0;
+  double gamma_ = pi;   
+
+  // define exact solution
+  // - \gamma/(\pi^2)*(p*\exp{\lambda_1*x} + (1-p)*\exp{\lambda_2*x} - 1)*sin(\pi*y)
+  double lambda1 = -alpha_/2 - std::sqrt((alpha_/2)*(alpha_/2) + pi*pi);
+  double lambda2 = -alpha_/2 + std::sqrt((alpha_/2)*(alpha_/2) + pi*pi);
+
+  double p_ = (1-std::exp(lambda2))/(std::exp(lambda1)-std::exp(lambda2));
+  
+  auto solutionExpr = [&gamma_, &lambda1, &lambda2, &p_](SVector<2> x) -> double { 
+    return -gamma_/(pi*pi) * ( p_ * std::exp(lambda1 * x[0]) + (1 - p_) * std::exp(lambda2 * x[0]) - 1. ) * std::sin(pi * x[1]);
+  };
+  
+  // non-zero forcing term
+  auto forcingExpr = [&gamma_](SVector<2> x) -> double { 
+    return gamma_ * std::sin(pi * x[1]);
+  };
+  ScalarField<2> forcing(forcingExpr); // wrap lambda expression in ScalarField object
+  
+  // differential operator
+  SVector<2> beta_;
+  beta_ << -alpha_, 0.;
+  auto bilinearForm = 1.0*Laplacian() + Gradient(beta_);
+  // load sample mesh for order 1 basis
+  MeshLoader<Mesh2D<2>> unit_square("unit_square");
+  
+  PDE<2,2,2, decltype(bilinearForm), ScalarField<2>> pde_(unit_square.mesh, bilinearForm, forcing);
+  
+  // compute boundary condition and exact solution
+  DMatrix<double> nodes_ = unit_square.mesh.dofCoords();
+  DMatrix<double> solution_ex(nodes_.rows(), 1);
+
+  for(int i=0; i<nodes_.rows(); ++i){
+    solution_ex(i) = solutionExpr(nodes_.row(i));
+  }
+  // set dirichlet conditions        
+  DMatrix<double> dirichletBC = DMatrix<double>::Zero(nodes_.rows(), 1);
+  pde_.setDirichletBC(dirichletBC);
+
+  // init solver and solve differential problem
+  pde_.init();
+  pde_.solve();
+  
+  // check computed error
+  DMatrix<double> error_ = solution_ex - pde_.solution();
+  double error_L2 = (pde_.R0() * error_.cwiseProduct(error_)).sum();
+  EXPECT_TRUE(error_L2 < 1e-7);
 }
