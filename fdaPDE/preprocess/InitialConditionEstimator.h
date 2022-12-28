c357087344d652b3b3000373c64d9fe75408bed1
AlePalu <alessandropalummo@gmail.com>
2022-12-28 17:22:17 +0100

    initial condition estimation adapts to the specific type of differential regularization taken from the requested parabolic model

--- fdaPDE/preprocess/InitialConditionEstimator.h
+++ fdaPDE/preprocess/InitialConditionEstimator.h
@@ -21,54 +21,48 @@ using fdaPDE::models::SRPDE;
 namespace fdaPDE{
 namespace preprocess {
 
-  // tries to build an estimation of the initial condition from the data at time step 0
-  // the estimation is obtained selecting the best (according to GCV) spatial field estimate obtained from
-  // an SRPDE model under simple laplacian regularization
+  // for a space-time regression model builds an estimation of the initial condition from the data at time step 0
+  // the estimation is obtained selecting the best spatial field estimate obtained from an SRPDE model via GCV optimization
   template <typename Model>
   class InitialConditionEstimator {
     static_assert(std::is_same<typename model_traits<Model>::RegularizationType, SpaceTimeParabolicTag>::value,
 		  "Initial condition estimation is for parabolic regularization only");
   private:
-    const Model& model_;
+    const Model& model_; // model to initialize
   public:
+    // constructor
     InitialConditionEstimator(const Model& model) : model_(model) {};
-
+    // builds the initial condition estimate
     DMatrix<double> apply(const std::vector<SVector<1>>& lambdas){
       // extract data at time step 0
       std::size_t n = model_.n_locs();
       BlockFrame<double, int> df = model_.data()(0, n-1).extract();
 
-      // define simple laplacian regularization term
-      auto L = Laplacian();
+      // cast space-time differential operator df/dt + Lf = u to space-only Lf = u
+      auto L = model_.pde().bilinearForm().remove_operator<dT>();
+      // prepare regularization term
       typedef PDE<Model::M, Model::N, Model::K, decltype(L), DMatrix<double>> PDE_;
       PDE_ problem(model_.domain());
       DMatrix<double> u = DMatrix<double>::Zero(problem.domain().elements()*problem.integrator().numNodes(), 1);
       problem.setBilinearForm(L);
       problem.setForcing(u);
-
-      problem.init();
+      problem.init(); // init PDE object
       // define SRPDE model for initial condition estimation
       SRPDE<PDE_, model_traits<Model>::sampling> m(problem, model_.locs());
-
-      // impose data
-      m.setData(df);
+      m.setData(df); // impose data
       
-      // define stochastic GCV calibrator
+      // define GCV calibrator
       ExactGCV<decltype(m)> calibrator(m);
       GridOptimizer<1> gcv_optimizer;
-      
       // find optimal smoothing parameter
       SVector<1> opt = calibrator.approx(gcv_optimizer, 0.01, lambdas);
-      // check if lambda is on border of domain
 
       // fit model with optimal lambda
       m.setLambda(opt[0]);
       m.solve();
-      
       // return initial condition estimate
       return m.f();
-    }
-    
+    }    
   };
   
 }}
