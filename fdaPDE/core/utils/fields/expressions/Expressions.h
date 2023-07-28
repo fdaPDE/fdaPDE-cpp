0b74f9985b392d1377656f127add3856510f98d5
AlePalu <alessandropalummo@gmail.com>
2023-07-28 11:03:01 +0200

    removed core (preparing for submodule)

--- fdaPDE/core/utils/fields/expressions/Expressions.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EXPRESSIONS_H__
-#define __EXPRESSIONS_H__
-
-// include the full expression template engine
-#include "ScalarExpressions.h"
-#include "VectorExpressions.h"
-#include "MatrixExpressions.h"
-
-#endif // __EXPRESSIONS_H__
