1b00536776ee815e27698c69084e7f993633d8d5
Marco Francesco De Sanctis <marcofrancesco99@gmail.com>
2024-01-20 18:27:19 +0000

    creation branch for space-time QSRPDE models

--- test/main.cpp
+++ test/main.cpp
@@ -3,25 +3,20 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 
-// // regression test suite
-// #include "src/srpde_test.cpp"
-// #include "src/strpde_test.cpp"
-// #include "src/gsrpde_test.cpp"
-// #include "src/qsrpde_test.cpp"
-// #include "src/gcv_srpde_test.cpp"
-// #include "src/gcv_qsrpde_test.cpp"
-// #include "src/gcv_srpde_newton_test.cpp"
-// #include "src/kcv_srpde_test.cpp"
-// // functional test suite
-// #include "src/fpca_test.cpp"
-// // regression test suites
-// #include "src/qstrpde_test.cpp"   
-// #include "src/mqsrpde_test.cpp"
-// GCV test suites
-// #include "src/gcv_qstrpde_test.cpp"  
-// #include "src/gcv_mqsrpde_test.cpp"
-// // Case study 
-#include  "src/case_study.cpp"
+// regression test suite
+#include "src/srpde_test.cpp"
+#include "src/strpde_test.cpp"
+#include "src/gsrpde_test.cpp"
+#include "src/qsrpde_test.cpp"
+#include "src/qstrpde_test.cpp" 
+#include "src/gcv_srpde_test.cpp"
+#include "src/gcv_qsrpde_test.cpp"
+#include "src/gcv_srpde_newton_test.cpp"
+#include "src/gcv_qstrpde_test.cpp" 
+#include "src/kcv_srpde_test.cpp"
+// functional test suite
+#include "src/fpca_test.cpp"
+
 
 int main(int argc, char **argv){
   // start testing
