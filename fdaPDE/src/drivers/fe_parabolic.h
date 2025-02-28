9082b2f9fbe8eda2470a69596c788cf3043d2797
AlePalu <alessandropalummo@gmail.com>
2025-02-28 12:54:28 +0100

    finite element parabolic driver, iterative scheme

--- fdaPDE/src/drivers/fe_parabolic.h
+++ fdaPDE/src/drivers/fe_parabolic.h
@@ -82,11 +82,11 @@ template <> class fe_parabolic_driver<monolithic> {
             L.makeCompressed();
             L_ = kronecker(L, R0__);
         }
-        // correct first n discretized force rows as (u_1 + R0*s / DeltaT)
         u_.resize(n_dofs_ * m_);
         {
             vector_t u = linear_form.assemble();
             for (int i = 0; i < m_; ++i) { u_.segment(i * n_dofs_, n_dofs_) = u; }
+            // correct first n discretized force rows as (u_1 + R0*s / DeltaT)
             u_.segment(0, n_dofs_) += (1.0 / DeltaT_) * (R0__ * s_);
         }
 	
@@ -267,10 +267,281 @@ template <> class fe_parabolic_driver<monolithic> {
 
 };
 
-// template <> struct fe_parabolic_driver_impl<iterative_tag> : fe_parabolic_driver_base {
+template <> struct fe_parabolic_driver<iterative> {
+   private:
+    using vector_t        = Eigen::Matrix<double, Dynamic, 1>;
+    using matrix_t        = Eigen::Matrix<double, Dynamic, Dynamic>;
+    using sparse_matrix_t = Eigen::SparseMatrix<double>;
+    using diag_matrix_t   = Eigen::DiagonalMatrix<double, Dynamic, Dynamic>;
+    using sparse_solver_t = Eigen::SparseLU<sparse_matrix_t>;
+    using dense_solver_t  = Eigen::PartialPivLU<matrix_t>;
 
-// };
-  
+    class block_map_t {
+        static constexpr int Order = 3;
+        using Scalar = double;
+        using storage_t = MdMap<Scalar, full_dynamic_extent_t<Order>, internals::layout_left>;
+
+        storage_t data_;
+        int rows_ = 0;
+        int blk_rows_ = 0, blk_cols_ = 0;   // single block size
+       public:
+        block_map_t() noexcept = default;
+        template <typename DataT>
+            requires(internals::is_eigen_dense_xpr_v<DataT> &&
+                     std::is_same_v<typename std::decay_t<DataT>::Scalar, double>)
+        block_map_t(DataT&& data, int rows, int blk_rows, int blk_cols) :
+            data_(data.data(), rows, data.cols(), (data.rows() / rows)),
+            rows_(rows),
+            blk_rows_(blk_rows),
+            blk_cols_(blk_cols) {
+            fdapde_assert(data.rows() % rows == 0 && rows % blk_rows == 0 && data.cols() % blk_cols == 0);
+        }
+        template <typename DataT>
+            requires(internals::is_eigen_dense_xpr_v<DataT> &&
+                     std::is_same_v<typename std::decay_t<DataT>::Scalar, double>)
+        block_map_t(DataT&& data, int rows) :   // divide data in (data.rows() / rows) blocks of size rows x data.cols()
+            data_(data.data(), rows, data.cols(), (data.rows() / rows)),
+            rows_(rows),
+            blk_rows_(rows),
+            blk_cols_(data.cols()) {
+            fdapde_assert(data.rows() % rows == 0);
+        }
+        // observers
+        auto operator()(int i, int j, int k) const {
+            auto slice_ = data_.template slice<2>(k);
+            return slice_.as_eigen_map().block(
+              i * blk_rows_, j * blk_cols_, (i + 1) * blk_rows_ - 1, (j + 1) * blk_cols_ - 1);
+        }
+        auto operator()(int j, int k) const {   // get j-th column of k-th time instant
+            auto slice_ = data_.template slice<2>(k);
+            return slice_.as_eigen_map().block(0, j * blk_cols_, rows_ - 1, (j + 1) * blk_cols_ - 1);
+        }
+        auto operator()(int k) const { return data_.template slice<2>(k).as_eigen_map(); }
+        int size() const { return data_.size(); }
+        int blk_size() const { return blk_rows_ * blk_cols_; }
+        int blk_rows() const { return blk_rows_; }
+        int blk_cols() const { return blk_cols_; }
+        const double* data() const { return data_.data(); }
+        // modifiers
+        double* data() { return data_.data(); }
+        auto operator()(int i, int j, int k) {
+            auto slice_ = data_.template slice<2>(k);
+            return slice_.as_eigen_map().block(
+              i * blk_rows_, j * blk_cols_, (i + 1) * blk_rows_ - 1, (j + 1) * blk_cols_ - 1);
+        }
+        auto operator()(int j, int k) {   // get j-th column of k-th time instant
+            auto slice_ = data_.template slice<2>(k);
+            return slice_.as_eigen_map().block(0, j * blk_cols_, rows_ - 1, (j + 1) * blk_cols_ - 1);
+        }
+        auto operator()(int k) { return data_.template slice<2>(k).as_eigen_map(); }
+    };
+
+    // J(f,g) = \sum_{k=1}^m (y^(k) - \Psi * f^(k))^\top * (y^(k) - \Psi * f^(k)) + \lambda_D * (g^(k))^\top * (g^(k))
+    double J_(const block_map_t& y, const block_map_t& f, const block_map_t& g, double lambda) const {
+        double sse = 0;
+        for (int t = 0; t < m_; ++t) { sse += ((y(t) - Psi_ * f(t)).squaredNorm() + lambda * g(t).squaredNorm()); }
+        return sse;
+    }
+
+    template <typename GeoFrame, typename Penalty, typename WeightMatrix>
+    void init_(const std::string& formula, const GeoFrame& gf, Penalty&& penalty, const WeightMatrix& W) {     
+        fdapde_static_assert(internals::is_valid_penalty_pair_v<Penalty>, INVALID_PENALTY_DESCRIPTION);
+	// enforce POINT indexing in time
+	fdapde_assert(gf.category(0)[1] == ltype::point);
+        using BilinearForm = std::tuple_element_t<0, std::decay_t<Penalty>>;
+        using LinearForm = std::tuple_element_t<1, std::decay_t<Penalty>>;
+        using FeSpace = typename BilinearForm::TrialSpace;
+	// discretization
+        const BilinearForm& bilinear_form = std::get<0>(penalty);
+        const LinearForm& linear_form = std::get<1>(penalty);
+        n_dofs_ = bilinear_form.n_dofs();
+        internals::fe_mass_assembly_loop<FeSpace> mass_assembler(bilinear_form.trial_space());
+        R0_ = mass_assembler.assemble();
+        R1_ = bilinear_form.assemble();
+	// extract time step and number of time instants
+        const auto& time_index = geo_index_cast<1, POINT>(gf[0]);
+        const auto& time_coords = time_index.coordinates();
+        m_ = time_coords.rows();
+        fdapde_assert(m_ > 0 && time_coords.cols() == 1);
+        DeltaT_ = time_coords(1, 0) - time_coords(0, 0);
+        for (int i = 1; i < m_ - 1; ++i) {
+            double lag_i = time_coords(i + 1, 0) - time_coords(i, 0);
+            fdapde_assert(DeltaT_ > 0 && lag_i > 0 && almost_equal(DeltaT_ FDAPDE_COMMA lag_i));
+        }
+        u_.resize(n_dofs_ * m_);
+        {
+            vector_t u = linear_form.assemble();
+            for (int i = 0; i < m_; ++i) { u_.segment(i * n_dofs_, n_dofs_) = u; }
+            // correct first n discretized force rows as (u_1 + R0*s / DeltaT)
+            u_.segment(0, n_dofs_) += (1.0 / DeltaT_) * (R0_ * s_);
+        }
+        // basis system evaluation
+        switch (gf.category(0)[0]) {
+        case ltype::point: {
+            const auto& spatial_index = geo_index_cast<0, POINT>(gf[0]);
+	    n_ = spatial_index.rows();
+            // evaluate basis at locations
+            Psi_ = internals::point_basis_eval(bilinear_form.trial_space(), spatial_index);
+            D_ = vector_t::Ones(n_).asDiagonal();
+            break;
+        }
+        case ltype::areal: {
+            const auto& spatial_index = geo_index_cast<0, POLYGON>(gf[0]);
+            n_ = spatial_index.rows();
+            const auto& [psi, measure_vec] =
+              internals::areal_basis_eval(bilinear_form.trial_space(), spatial_index);
+            Psi_ = psi;
+            vector_t D(n_obs_);
+            for (int i = 0; i < m_; ++i) { D.segment(i * measure_vec.rows(), measure_vec.rows()) = measure_vec; }
+            D_ = D.asDiagonal();
+            break;
+        }
+        }
+        // data extraction
+        Formula formula_(formula);
+        std::vector<std::string> covs;
+        for (const std::string& token : formula_.rhs()) {
+            if (gf.contains(token)) { covs.push_back(token); }
+        }
+        n_covs_ = covs.size();
+        y_.resize(n_obs_);
+        {
+            const auto& y_data = gf[0].data().template col<double>(formula_.lhs());
+            y_data.assign_to(y_);
+            if (y_data.has_nan()) {   // correct \Psi for missing observations
+                Psi_ = y_data.nan().as_matrix().repeat(1, n_dofs_).select(Psi_);
+            }
+        }
+        return;
+    }
+   public:
+    using solution_policy = iterative;
+
+    fe_parabolic_driver() noexcept = default;
+    template <typename GeoFrame, typename Penalty, typename InitialCondition, typename WeightMatrix>
+        requires(internals::is_pair_v<Penalty>)
+    fe_parabolic_driver(
+      const std::string& formula, const GeoFrame& gf, Penalty&& penalty, const InitialCondition& s,
+      const WeightMatrix& W, double tol, int max_iter) :
+        s_(s), tol_(tol), max_iter_(max_iter) {
+        fdapde_static_assert(GeoFrame::Order == 2, THIS_CLASS_IS_FOR_ORDER_TWO_GEOFRAMES_ONLY);
+        n_obs_ = gf[0].rows();   // number of data locations on physical domain
+        init_(formula, gf, penalty, W);
+    }
+    template <typename GeoFrame, typename Penalty, typename InitialCondition, typename WeightMatrix>
+        requires(internals::is_pair_v<Penalty>)
+    fe_parabolic_driver(
+      const std::string& formula, const GeoFrame& gf, Penalty&& penalty, const InitialCondition& s,
+      const WeightMatrix& W) :
+        fe_parabolic_driver(formula, gf, penalty, s, W, 1e-4, 50) { }
+    template <typename GeoFrame, typename Penalty, typename InitialCondition>
+        requires(internals::is_pair_v<Penalty>)
+    fe_parabolic_driver(const std::string& formula, const GeoFrame& gf, Penalty&& penalty, const InitialCondition& s) :
+        fe_parabolic_driver(
+          formula, gf, penalty, s, Eigen::Matrix<double, Dynamic, 1>::Ones(gf[0].rows()).asDiagonal(), 1e-4, 50) { }
+
+    void operator()(double lambda_D, double lambda_T) {
+        // define auxiliary storage
+        f_.resize(n_dofs_ * m_, y_.cols());
+        g_.resize(n_dofs_ * m_, y_.cols());
+        matrix_t f_old_buff(n_dofs_ * m_, y_.cols()), g_old_buff(n_dofs_ * m_, y_.cols());
+        // map to space-time structures
+        block_map_t y(y_, n_);
+        block_map_t u(u_, n_dofs_);
+        block_map_t f_old(f_old_buff, n_dofs_), f_new(f_, n_dofs_);
+        block_map_t g_old(g_old_buff, n_dofs_), g_new(g_, n_dofs_);
+        double alpha = lambda_D * lambda_T / DeltaT_;
+	
+        // compute starting point (f^(k,0), g^(k,0)) k = 1 ... m
+        {
+            SparseBlockMatrix<double, 2, 2> A_(
+              Psi_.transpose() * D_ * Psi_, lambda_D * R1_.transpose(), lambda_D * R1_, -lambda_D * R0_);
+            invA_.compute(A_);
+            vector_t b_(2 * n_dofs_);
+            for (int t = 0; t < m_; ++t) {
+                b_ << Psi_.transpose() * D_ * y(t), lambda_D * lambda_T * u(t);
+                f_old(t) = invA_.solve(b_).head(n_dofs_);
+            }
+	    
+            sparse_matrix_t G0 = alpha * R0_.transpose() + lambda_D * R1_.transpose();
+            sparse_solver_t invG0;
+            invG0.compute(G0);
+            b_ = Psi_.transpose() * D_ * (y(m_ - 1) - Psi_ * f_old(m_ - 1));   // g^(t + 1,0) = 0
+            g_old(m_ - 1) = invG0.solve(b_);
+            // general step
+            for (int t = m_ - 2; t >= 0; --t) {
+                b_ = Psi_.transpose() * D_ * (y(t) - Psi_ * f_old(t)) + alpha * R0_ * g_old(t + 1);
+                g_old(t) = invG0.solve(b_);
+            }
+        }
+
+        // iterative scheme initialization
+        double Jold = std::numeric_limits<double>::max();
+        double Jnew = J_(y, f_old, g_old, lambda_D);
+        int i = 1;
+        SparseBlockMatrix<double, 2, 2> A_(
+          Psi_.transpose() * D_ * Psi_, lambda_D * R1_.transpose() + alpha * R0_.transpose(),
+          lambda_D * R1_ + alpha * R0_, -lambda_D * R0_);
+        invA_.compute(A_);
+	vector_t b_(2 * n_dofs_);
+        // start loop
+	Eigen::Matrix<double, Dynamic, 1> x;
+        while (i < max_iter_ && std::abs((Jnew - Jold) / Jnew) > tol_) {
+            b_ << Psi_.transpose() * D_ * y(0) + alpha * R0_ * g_old(1), lambda_D * u(0);
+	    x = invA_.solve(b_);
+	    f_new(0) = x.topRows(n_dofs_);
+	    g_new(0) = x.bottomRows(n_dofs_);
+            // general step
+            for (int t = 1; t < m_ - 1; ++t) {
+                b_ << Psi_.transpose() * D_ * y(t) + alpha * R0_ * g_old(t + 1),
+                  alpha * R0_ * f_old(t - 1) + lambda_D * u(t);
+                x = invA_.solve(b_);
+                f_new(t) = x.topRows(n_dofs_);
+                g_new(t) = x.bottomRows(n_dofs_);
+            }
+            // at last step g^(k+1,i-1) is zero
+            b_ << Psi_.transpose() * D_ * y(m_ - 1), alpha * R0_ * f_old(m_ - 2) + lambda_D * u(m_ - 1);
+            x = invA_.solve(b_);
+            f_new(m_ - 1) = x.topRows(n_dofs_);
+            g_new(m_ - 1) = x.bottomRows(n_dofs_);
+            // prepare for next iteration
+            Jold = Jnew;
+            f_old = f_new;
+            g_old = g_new;
+            Jnew = J_(y, f_old, g_old, lambda_D);
+            i++;
+        }
+        return;
+    }
+    // observers
+    int n_dofs() const { return n_dofs_; }
+    const sparse_matrix_t& R0() const { return R0_; }
+    const sparse_matrix_t& R1() const { return R1_; }
+    const sparse_matrix_t& Psi() const { return Psi_; }
+    const vector_t& u() const { return u_; }
+    const vector_t& f() const { return f_; }
+    const vector_t& beta() const { return beta_; }
+    const vector_t& g() const { return g_; }
+   protected:
+    int n_dofs_ = 0, n_obs_ = 0, n_covs_ = 0;
+    double DeltaT_ = 0;
+    sparse_matrix_t R0_;    // n_dofs x n_dofs matrix [R0]_{ij} = \int_D \psi_i * \psi_j
+    sparse_matrix_t R1_;    // n_dofs x n_dofs matrix [R1]_{ij} = \int_D a(\psi_i, \psi_j)
+    sparse_matrix_t Psi_;   // n_obs x n_dofs matrix [Psi]_{ij} = \psi_j(p_i)
+    vector_t u_;            // (n_dofs * m) x 1 vector u = [u_1 + + R0_*s / DeltaT, u_2, \ldots, u_n]
+    diag_matrix_t D_;       // vector of regions' measures (areal sampling)
+    mutable std::optional<sparse_solver_t> invR0_;
+    vector_t f_, beta_, g_;
+    sparse_solver_t invA_;   // factorization of (2 * n_dofs) x (2 * n_dofs) nonparametric matrix
+    vector_t s_;             // initial condition vector
+
+    int n_, m_;    // number of spatial and temporal locations
+    vector_t y_;   // n_obs x 1 observation vector
+
+    int max_iter_ = 50;   // maximum number of iterations
+    double tol_ = 1e-4;   // convergence tolerance
+};
+
 }   // namespace internals
 }   // namespace fdapde
 
